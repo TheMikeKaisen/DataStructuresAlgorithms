70e38a37ebb2202b00b0365d7461b31642b75eea
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2023-06-29 23:13:02 +0530

    Day 3

---
+++ 3_Recursion/8_sumDigits.cpp
@@ -0,0 +1,18 @@
+// program to get sum of digits 
+
+#include<iostream>
+using namespace std;
+
+// time complexity = Theta(number of digits)
+// auxiliary space = Theta(1)
+int sum(int n, int k = 0){
+    if(n/10==0){
+        return (k+n);
+    }
+    return sum(n/10, k+(n%10));
+}
+
+int main(){
+    cout << sum(567) << endl;
+    return 0;
+}
