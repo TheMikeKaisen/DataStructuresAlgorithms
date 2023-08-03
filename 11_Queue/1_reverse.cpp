a17870137c8b1e35678344f70c3255a3e9bd1cfd
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2023-08-03 21:53:34 +0530

    Day 27

---
+++ 11_Queue/1_reverse.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+
+queue<int> rev(queue<int> q)
+    {
+        stack<int> s;
+        queue<int> a;
+        for(int i = 0; i<=q.size(); i++){
+            s.push(q.front());
+            q.pop();
+        }
+        while(!s.empty()){
+            q.push(s.top());
+            s.pop();
+        }
+        return q;
+    }
+int main(){
+    queue<int> q;
+    q.push(4);
+    q.push(3);
+    q.push(1);
+    q.push(10);
+    q.push(2);
+    q.push(6);
+    cout << rev(q) << endl;
+    return 0;
+}
